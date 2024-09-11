384c378e69a8fff95643cea1a8b1b45e7c14e2e5
Favour Emmanuel <iyenefavour@gmail.com>
2024-09-11 21:34:26 +0100

    added todo component on the todoWrapper

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,16 @@
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 export const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <div className="Todo">
+      <p>Go to school</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
+    </div>
+  );
 };
